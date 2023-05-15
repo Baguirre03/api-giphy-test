d4ed8f2ca823ffca951fc9ec00390d6277aea448
Ben Aguirre <ben.aguirre03@gmail.com>
2023-05-15 14:08:13 -0500

    refactor with async/await

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ const img = document.querySelector('img')
 const searchBtn = document.querySelector('#search-button')
 const input = document.querySelector('#search-input')
 
+
 async function searchGif(search) {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=STRPA5sB9XUZJ1mZDvdcx3o4AKSUovJC&s=${search}`, {mode: 'cors'})
-        .then(response => response.json())
-        .then(response => img.src = response.data.images.original.url)
-        .catch(function(err) {
-            defaultImage(err)
-            throw new Error(err)
-        })
+    try {
+        const result = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=STRPA5sB9XUZJ1mZDvdcx3o4AKSUovJC&s=${search}`, {mode: 'cors'})
+        const apiData = await result.json()
+        img.src = apiData.data.images.original.url
+    } catch(error) {
+        defaultImage()
+        throw new Error(error)
+    }
+    // fetch(`https://api.giphy.com/v1/gifs/translate?api_key=STRPA5sB9XUZJ1mZDvdcx3o4AKSUovJC&s=${search}`, {mode: 'cors'})
+    //     .then(response => response.json())
+    //     .then(response => img.src = response.data.images.original.url)
+    //     .catch(function(err) {
+    //         defaultImage(err)
+    //         throw new Error(err)
+    //     })
 }
 
-function defaultImage() {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=STRPA5sB9XUZJ1mZDvdcx3o4AKSUovJC&s=cat`, {mode: 'cors'})
-        .then(response => response.json())
-        .then(response => img.src = response.data.images.original.url)
-        .catch(function(err) {
-            throw new Error(err)
-        })
+async function defaultImage() {
+    try {
+        const result = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=STRPA5sB9XUZJ1mZDvdcx3o4AKSUovJC&s=cat`, {mode: 'cors'})
+        const catData = await result.json()
+        img.src = catData.data.images.original.url
+    } catch(err) {
+        throw new Error(err)
+    }
 }
 
 searchBtn.addEventListener('click', (e) => {
